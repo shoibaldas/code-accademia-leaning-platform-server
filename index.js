2e2ab09b909b5c4b7b1161e99e6783b1b2197445
shoibaldas <shoibalshantu@gmail.com>
2022-10-26 04:14:41 +0600

    change in jason data and getting data by id in index.js

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ app.get('/category-courses', (req, res) => {
     res.send(categories);
 });
 
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const getCourse = categories.find(n => n._id === id);
+    res.send(getCourse);
+});
 
 app.listen(port, () => {
     console.log("News Agency Server is Running...", port);
